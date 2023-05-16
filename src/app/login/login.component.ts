ab0cdda0ec2e2da9e2e47c48e86ae9c9be16594f
Gus Vendegna <augustvendegna@mines.edu>
2023-05-16 17:58:08 -0600

    added much CSS and attempted to connect to SQL database, but left it commented out

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   public username: string;
   public password: string;
+  // const { Pool, Client } = require("pg"); // idk what that is
+  //Client = require("pg");
+  dbCreds = {
+    user: "postgres",
+    host: "localhost",
+    database: "users",
+    port: 5432,
+  };
 
   constructor(){
   }
@@ -16,7 +24,17 @@ export class LoginComponent {
     
     console.log('Username: ' + this.username);
     console.log('Password: ' + this.password);
+    //this.writeCreds();
   }
+
+  // async writeCreds(){
+  //   const client = new this.Client(this.dbCreds);
+  //   await client.connect();
+  //   const now = await client.query("SELECY * FROM users");
+  //   await client.end();
+  //   console.log(now);
+  //   return now;
+  // }
 
 
 }
