5aef820e3ef8b88c6ae85567929caf750834d5cd
Gus Vendegna <112891253+augustvendegna@users.noreply.github.com>
2023-05-16 18:29:33 +0000

    sample login page complete - bare ass

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  public username: string;
+  public password: string;
+
+  constructor(){
+  }
+  
+  public login() {
+    
+    console.log('Username: ' + this.username);
+    console.log('Password: ' + this.password);
+  }
+
+
+}
